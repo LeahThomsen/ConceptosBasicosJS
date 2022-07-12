2e0666156d3d2a628e07483ea7aa7ddaed045085
Leah <leahthomsen@gmail.com>
2022-07-12 16:35:48 -0300

    learning to switch between branches and TP2 ej3

---
+++ js/4-switch.js
@@ -0,0 +1,48 @@
+// se usa cuando tenemos programas que simulan un menu y reemplaza a una serie de if anidados
+
+let estacion = prompt('Seleccione su estacion del year preferida: 1- verano, 2- primavera, 3-autumn, 4-invierno');
+
+// if( estacion === '1')
+document.write(Sushi');
+}else{
+    if(estacion === "2"){
+        document.write('Sandwich de milanesa');
+    }else if ( estacion === '3'){
+        document.write('fideos con manteca y queso');
+    }else if ( estacion === '4'){
+        document.write('Guiso de fideo monito');
+    }else{
+        document.write('ingreso una opcion incorrecta');
+    }
+}
+git
+/*switch(opcion){
+    case '1':
+        todas las lineas de codigo
+        break;
+  case '2':
+        todas las lineas de codigo
+        break; 
+        
+        
+case 'n':
+        todas las lineas de codigo
+        break;
+        default:'
+        todas las lineas de codigo e ejecutar si no se cumplio ningun case
+}*/
+
+switch(estacion){
+    case '1':
+        document.write('Sushi');
+    case '2':
+        document.write('Sandwich de milanesa');
+    case '3':
+        document.write('Fideo con manteca y queso');')   
+    case '4':
+        document.write('guiso con fideo monito');
+        break;
+        default:
+        document.write(ingreso una opcion incorrecta');     
+
+}
---
+++ js/ejercicio/tp-2-ej3.js
@@ -0,0 +1,13 @@
+//pedir texto al usuario hasta que presione cancelar
+
+//while(confirm('?Desea ingresar palabras?')){
+
+let fraseCompleta=''
+let textIngresado = prompt('Ingrese una palabra');
+fraseCompleta = fraseCompleta + textoIngresado
+console.log(textIngresado)
+console.log(fraseCompleta)
+}
+
+
+//una vez que cancelo muestro todo el texto separado con -
