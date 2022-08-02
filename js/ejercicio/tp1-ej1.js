6af0c23edb1607ca177b92a9a90db9167b0c7e91
Leah <leahthomsen@gmail.com>
2022-08-02 14:46:17 -0300

    tp1-ej1.js

--- js/ejercicio/tp1-ej1.js
+++ js/ejercicio/tp1-ej1.js
@@ -1,12 +1,6 @@
 //Escribe un programa de una sola línea que haga que aparezca en la pantalla un alert que diga “un mensaje”.
-//
 
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Ejemplo JavaScript</title>
-  </head>
-  <body>
-   <button onclick="alert('Un Mensaje')">Mostrar Mensaje</button>
-  </body>
-</html>	
+alert( un mensaje );
+
+
+
